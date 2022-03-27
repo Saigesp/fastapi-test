eb82c9d2ae7499b5e1b3cbeb36f6f13a1ad34104
Saigesp <saigesp@gmail.com>
2022-03-27 14:21:47 +0200

    chore: rename geometries api fucntion

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,5 @@
 from .root import get_root
-from .geometries import get_postal_codes
+from .postal_codes import get_postal_codes
 from .paystats import (
     get_paystats_by_age_gender,
     get_paystats_by_time,
